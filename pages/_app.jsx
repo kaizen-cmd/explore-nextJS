d2bd86b89cdb7c913034e0d3daed800f70355154
Tejas Mandre <tejasmandre0306@gmail.com>
2020-08-21 01:39:04 +0530

    pwa config added

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -60,6 +60,31 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <title>CodeStrike</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/ico.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/ico.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/ico.png"></link>
+        <meta name="theme-color" content="#317EFB" />
         <link
           rel="stylesheet"
           href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
