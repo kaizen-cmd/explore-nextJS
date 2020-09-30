83a50488d549ef059b00708bef4d5b301f6f4d43
Tejas Mandre <tejasmandre0306@gmail.com>
2020-10-01 01:14:19 +0530

    ad sense code added

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -93,6 +93,11 @@ export default function MyApp({ Component, pageProps }) {
           integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
           crossOrigin="anonymous"
         ></script>
+        <script
+          data-ad-client="ca-pub-8760818264352491"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        ></script>
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
