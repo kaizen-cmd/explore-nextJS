00068c34a732f589fec67623f8540f5ef4529aa4
Tejas Mandre <tejasmandre0306@gmail.com>
2020-10-11 19:51:41 +0530

    course card 6 responsive

--- components/miscellaneous/course-card-6.jsx
+++ components/miscellaneous/course-card-6.jsx
@@ -2,23 +2,23 @@ import Link from "next/link";
 
 const CourseCard6 = ({ title, description, link }) => {
   return (
-    <div className="col-lg-6 px-2">
+    <div className="col-lg-6 px-2 mb-2">
       <div
         className="shadow p-3 text-light"
         style={{
           backgroundColor: "#133b5c",
-          height: "230px",
+          // height: "250px",
         }}
       >
         <div className="d-flex align-items-center h-100 justify-content-center flex-column">
           <div className="mb-2">
-            <h2 className="font-weight-bold m-0 mb-2">{title}</h2>
+            <h2 className="font-weight-bold m-0 mb-3">{title}</h2>
             <p className="m-0" style={{
                 wordBreak: "break-word"
             }}>{description}</p>
           </div>
           <Link href={link}>
-            <div className="w-100">
+            <div className="w-100 mt-1">
               <a className="btn btn-md border-light text-light w-100 py-2">
                 Practice Now
               </a>
