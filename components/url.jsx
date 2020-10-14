cadd3778a984ae30f38ff637576be5f9607e6dba
Tejas Mandre <tejasmandre0306@gmail.com>
2020-10-14 13:39:12 +0530

    debug resolved

--- components/url.jsx
+++ components/url.jsx
@@ -3,5 +3,5 @@ const DEBUG = false;
 
 var URL;
 
-DEBUG ? (URL = "https://api.codestrike.in") : (URL = "http://localhost:8000");
+!DEBUG ? (URL = "https://api.codestrike.in") : (URL = "http://localhost:8000");
 export default URL;
