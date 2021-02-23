27ae8c6394809b43596190cf1dfed6b6e2cbf94a
SuyashMuley <59868486+SuyashMuley@users.noreply.github.com>
2021-02-23 19:28:57 +0530

    LoggedIn added for Google Form MCQ

--- pages/googleform/pretestMCQ.jsx
+++ pages/googleform/pretestMCQ.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import BaseLayout from "../../components/base_layout";
+import { loggedIn } from "../_app";
 
 const GoogleForm = (props) => {
   return (
@@ -14,16 +15,18 @@ const GoogleForm = (props) => {
       <div className="container ">
         <div className="col-lg-full d-flex flex-column align-items-center mb-5">
           <div className="d-flex justify-content-center">
-          <iframe 
-            src="https://docs.google.com/forms/d/e/1FAIpQLSf0HBWcT4daCzFopkd8Ppg-TpGW1inkO4vHapXpJAjuPV1yzw/viewform?embedded=true" 
-            width="1280" 
-            height="960" 
-            frameBorder="0" 
-            marginHeight="0" 
-            marginWidth="0"
-            >
-              Loading…
+          {loggedIn &&
+            <iframe 
+              src="https://docs.google.com/forms/d/e/1FAIpQLSf0HBWcT4daCzFopkd8Ppg-TpGW1inkO4vHapXpJAjuPV1yzw/viewform?embedded=true" 
+              width="1280" 
+              height="960" 
+              frameBorder="0" 
+              marginHeight="0" 
+              marginWidth="0"
+              >
+                Loading…
             </iframe>
+          }
           </div>
         </div>
       </div>
