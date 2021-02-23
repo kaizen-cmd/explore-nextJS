d55a46cc993ccf6b83fe7f1ffafc697437236d77
SuyashMuley <59868486+SuyashMuley@users.noreply.github.com>
2021-02-23 20:25:05 +0530

    Path changed from Google form to MITSOE

--- components/new-user-homepage/dashboard.jsx
+++ components/new-user-homepage/dashboard.jsx
@@ -122,11 +122,11 @@ const Dashboard = (props) => {
 
                     <div>
                       <h3>
-                       Final Year MIT SOE Students Please Click Here for the MCQ Pretest
+                       Final Year MIT SOE Students please Click Here for the MCQ Pretest
                       </h3>
                     <div className="mt-2">
                        <a
-                        href="https://codestrike.in/googleform/pretestMCQ"
+                        href="https://codestrike.in/MITSOE/pretestMCQ"
                         target="_blank"
                         className="btn bnt-lg cc-btn font-weight-bold bg-success text-light"
                       >
