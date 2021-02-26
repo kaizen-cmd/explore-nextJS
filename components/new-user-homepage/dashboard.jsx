8862792f87b6a79b853e0274ad5dd0c78ca23435
SuyashMuley <59868486+SuyashMuley@users.noreply.github.com>
2021-02-26 16:35:23 +0530

    Dashboard Changes

--- components/new-user-homepage/dashboard.jsx
+++ components/new-user-homepage/dashboard.jsx
@@ -126,7 +126,7 @@ const Dashboard = (props) => {
                       </h3>
                     <div className="mt-2">
                        <a
-                        href="https://codestrike.in/MITSOE/test"
+                        href="https://codestrike-git-testing-kaizen11.vercel.app/MITSOE/te"
                         target="_blank"
                         className="btn bnt-lg cc-btn font-weight-bold bg-success text-light"
                       >
