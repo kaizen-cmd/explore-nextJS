10fa4ca9ca7fdbf2cae65f0dc18eee0b800b54ff
Tejas Mandre <tejasmandre0306@gmail.com>
2021-03-04 22:33:23 +0530

    adv-java-cpp-py mcqs added

---
+++ pages/MITSOE/adv-java-py-cpp.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import Head from "next/head";
+import BaseLayout from "../../components/base_layout";
+import { loggedIn } from "../_app";
+
+const Cform = (props) => {
+  useEffect(() => {
+    if (!loggedIn) {
+      setTimeout(() => {
+        document.getElementById("login-btn").click();
+      }, 1000);
+    }
+  });
+  return (
+    <BaseLayout>
+      <Head>
+        <meta
+          name="description"
+          content="CodeStrike is an online community of coders."
+        />
+      </Head>
+      <div className="container ">
+        <div className="col-lg-full d-flex flex-column align-items-center mb-5">
+          <div className="d-flex justify-content-center">
+            {loggedIn && (
+              <iframe
+                src="https://docs.google.com/forms/d/e/1FAIpQLSc_4L5TWNiMrzaNDW-qEmOKb6tjQwJND7icUZty1FMezMDwxA/viewform?embedded=true"
+                width="1280"
+                height="960"
+                frameBorder="0"
+                marginHeight="0"
+                marginWidth="0"
+              >
+                Loading…
+              </iframe>
+            )}
+          </div>
+        </div>
+      </div>
+    </BaseLayout>
+  );
+};
+
+export default Cform;
