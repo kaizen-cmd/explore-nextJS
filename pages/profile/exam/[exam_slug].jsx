3381bee705a0924e8bee7a1a9644cc83b0c4bcec
SuyashMuley <suyashmuley5@gmail.com>
2021-04-17 11:23:18 +0530

    view exam solutions

--- pages/profile/exam/[exam_slug].jsx
+++ pages/profile/exam/[exam_slug].jsx
@@ -1,1 +1,11 @@
 import parser from "html-react-parser";
+import BaseLayout from "../components/base_layout";
+import Head from "next/head";
+
+const examSlug = () => {
+    return(
+        <head>Hello</head>
+    )
+}
+
+export default examSlug;
