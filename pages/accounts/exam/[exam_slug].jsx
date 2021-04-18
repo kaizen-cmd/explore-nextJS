c2aad5f560173191b686b84121beb74f724038ec
Hardik Ambati <hardikambati69@gmail.com>
2021-04-18 21:51:44 +0530

    added test auth token + data to exam_slug

--- pages/accounts/exam/[exam_slug].jsx
+++ pages/accounts/exam/[exam_slug].jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import parser from "html-react-parser"
 import BaseLayout from "../../../components/base_layout";
 
@@ -14,35 +15,36 @@ const ExamSlug = (props) => {
         marginRight: "20%",
         borderRadius: '5px',
       }}>
+        {/* {props.mcq_objs.map((mcq) =>  */}
+          <div
+            style={{
+              border: "1px solid black",
+              margin: "5%",
+              borderRadius: '5px',
+              backgroundColor: "#f1f2f6"
+            }}>
+              <div 
+                style={{
+                  margin: "3%",
+                  marginBottom: "0%",
+                  fontSize: "105%",
+                  fontWeight: "600",
+                }}>
+                Q. Runtime polymorphism feature in java isQ. Runtime polymorphism feature in java is
+              </div>
 
-        <div
-          style={{
-            border: "1px solid black",
-            margin: "5%",
-            borderRadius: '5px',
-            backgroundColor: "#f1f2f6"
-          }}>
-            <div 
-              style={{
-                margin: "3%",
-                marginBottom: "0%",
-                fontSize: "105%",
-                fontWeight: "600",
-              }}>
-              Q. Runtime polymorphism feature in java isQ. Runtime polymorphism feature in java is
-            </div>
-
-            <div 
-              style={{
-                marginBottom: "3%",
-                marginLeft: "3%",
-                marginTop: "1%",
-                fontSize: "100%",
-                fontWeight: "500",
-              }}>
-              Ans: Hello World
-            </div>
-        </div> 
+              <div 
+                style={{
+                  marginBottom: "3%",
+                  marginLeft: "3%",
+                  marginTop: "1%",
+                  fontSize: "100%",
+                  fontWeight: "500",
+                }}>
+                Ans: Hello World
+              </div>
+          </div> 
+        {/* )} */}
     </div>
     
     </BaseLayout>
@@ -51,9 +53,14 @@ const ExamSlug = (props) => {
 
 export default ExamSlug;
 
-// ExamSlug.getInitialProps = async (ctx) => {
-//   return {
-//     leader_objs: data,
-//     cLink: "https://codestrike.in" + ctx.asPath,
-//   };
-// };
+ExamSlug.getInitialProps = async (ctx) => {
+  const auth = {
+    headers: {
+      'Authorization': "Token c386ea9c041d4ba6f070dc05fa82ec328c2b85ec"
+    }
+  }
+  const res = await axios.get('http://api.codestrike.in/mcqexam/user/attempted-exams/infytq-java-test-1/', auth);
+  return {
+    mcq_objs: res.data
+  };
+};
