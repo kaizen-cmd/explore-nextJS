0b4802ab23336804b96088c3ef7619a533a4f6c5
SuyashMuley <suyashmuley5@gmail.com>
2021-04-27 15:15:55 +0530

    Available Coding Exams added

---
+++ pages/exam/test.jsx
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import PaleBlueContainer from "../../components/miscellaneous/pale-blue-container";
+import URL from "../../components/base_layout";
+import BaseLayout from "../../components/base_layout";
+import WinnerCard from "../../components/new-user-homepage/winner-card";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Codeindex = () => {
+  const [availableCodeExam, setAvailableExams] = useState([]);
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      axios
+        .get('https://api.codestrike.in/codeportal/available-contests/', {
+          headers: { Authorization: localStorage.getItem("token") },
+        })
+        .then((res) => {
+          // console.log(res.data.res)
+          setAvailableExams(res.data.res);
+          // return res.data
+        });
+    } else {
+      router.push("/");
+    }
+  }, []);
+  return (
+    <BaseLayout>
+      <Head>
+        <title>Coding Test | CodeStrike</title>
+      </Head>
+      <PaleBlueContainer text="Coding Test"></PaleBlueContainer>
+      <div className="container">
+        <div className="row mb-4">
+          {availableCodeExam.map((exam, index) => {
+            return (
+              <WinnerCard
+                key={index}
+                link={`https://codestrike.in/contest/${exam.slug}/`}
+                bio={
+                  <div>
+                    <p>{exam.title}</p>
+                    <p>Click here</p>
+                  </div>
+                }
+                image={""}
+                newPage={false}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </BaseLayout>
+  );
+};
+
+export default Codeindex;
